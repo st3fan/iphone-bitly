01402f2bebac267777820cdfda2903b5521a333a
Stefan Arentz <stefan@arentz.ca>
2010-05-06 22:53:29 -0400

    Initial import

---
+++ Sources/URLShortener.h
@@ -0,0 +1,49 @@
+/*
+ * (C) Copyright 2010, Stefan Arentz, Arentz Consulting Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <Foundation/Foundation.h>
+
+@class URLShortener;
+
+@protocol URLShortenerDelegate
+- (void) shortener: (URLShortener*) shortener didSucceedWithShortenedURL: (NSURL*) shortenedURL;
+- (void) shortener: (URLShortener*) shortener didFailWithStatusCode: (int) statusCode;
+- (void) shortener: (URLShortener*) shortener didFailWithError: (NSError*) error;
+@end
+
+@interface URLShortener : NSObject {
+  @private
+	id<URLShortenerDelegate> _delegate;
+	NSString* _login;
+	NSString* _key;
+	NSURL* _url;
+  @private
+	NSURLConnection* _connection;
+	NSInteger _statusCode;
+	NSMutableData* _data;
+}
+
+@property (nonatomic,retain) id<URLShortenerDelegate> delegate;
+@property (nonatomic,retain) NSString* login;
+@property (nonatomic,retain) NSString* key;
+@property (nonatomic,retain) NSURL* url;
+
+- (void) execute;
+
+@end
---
+++ Sources/URLShortener.m
@@ -0,0 +1,106 @@
+/*
+ * (C) Copyright 2010, Stefan Arentz, Arentz Consulting Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "URLShortener.h"
+
+@implementation URLShortener
+
+@synthesize
+	delegate = _delegate,
+	key = _key,
+	login = _login,
+	url = _url;
+
+- (NSString*) _formEncodeString: (NSString*) string
+{
+	NSString* encoded = (NSString*) CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+		(CFStringRef) string, NULL, CFSTR("!*'();:@&=+$,/?%#[]"), kCFStringEncodingUTF8);
+	return [encoded autorelease];
+}
+
+#pragma mark -
+
+- (void) execute
+{
+	if (_connection == nil)
+	{
+		_data = [NSMutableData new];
+		
+		NSString* urlString = [NSString stringWithFormat: @"http://api.bit.ly/v3/shorten?login=%@&apiKey=%@&uri=%@&format=txt",
+			[self _formEncodeString: _login],
+			[self _formEncodeString: _key],
+			[self _formEncodeString: [_url absoluteString]]];
+			
+		NSURLRequest* request = [NSURLRequest requestWithURL: [NSURL URLWithString: urlString]
+			cachePolicy: NSURLRequestReloadIgnoringLocalCacheData timeoutInterval: 30.0];
+		_connection = [[NSURLConnection connectionWithRequest: request delegate: self] retain];
+	}
+}
+
+#pragma mark -
+
+- (void) connection: (NSURLConnection*) connection didReceiveData: (NSData*) data
+{
+	[_data appendData: data];
+}
+
+- (void)connection: (NSURLConnection*) connection didReceiveResponse: (NSHTTPURLResponse*) response
+{
+	_statusCode = [response statusCode];
+}
+
+- (void) connection: (NSURLConnection*) connection didFailWithError: (NSError*) error
+{
+	[_delegate shortener: self didFailWithError: error];
+
+	[_connection release];
+	_connection = nil;
+
+	[_data release];
+	_data = nil;
+}
+
+- (void) connectionDidFinishLoading: (NSURLConnection*) connection
+{
+	if (_statusCode != 200) {
+		[_delegate shortener: self didFailWithStatusCode: _statusCode];
+	} else {
+		NSString* string = [[[NSString alloc] initWithData: _data encoding: NSASCIIStringEncoding] autorelease];
+		string = [string stringByTrimmingCharactersInSet: [NSCharacterSet newlineCharacterSet]];
+		[_delegate shortener: self didSucceedWithShortenedURL: [NSURL URLWithString: string]];
+	}
+
+	[_connection release];
+	_connection = nil;
+	
+	[_data release];
+	_data = nil;
+}
+
+#pragma mark -
+
+- (void) dealloc
+{
+	[_key release];
+	[_login release];
+	[_url release];
+	[super dealloc];
+}
+
+@end
